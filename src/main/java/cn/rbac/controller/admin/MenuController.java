2cd67d5b68e18e89a18718279e1bd775b4362772
PushBug <3274773833@qq.com>
2023-12-16 20:58:22 +0800

    fix:取消拦截静态资源

--- src/main/java/cn/rbac/controller/admin/MenuController.java
+++
@@ -1,7 +0,0 @@
-package cn.rbac.controller.admin;
-
-/**
- * @Description 菜单功能
- */
-public class MenuController {
-}
--- src/main/java/cn/rbac/controller/admin/RoleController.java
+++
@@ -1,7 +0,0 @@
-package cn.rbac.controller.admin;
-
-/**
- * @Description 角色功能
- */
-public class RoleController {
-}
